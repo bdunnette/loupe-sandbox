a39334a18b7eb4db81467c854cab772731b94d4b
Brian Dunnette <dunn0172@umn.edu>
2017-06-26 09:54:05 -0500

    adding loupe

--- loupe_sandbox/settings.py
+++ loupe_sandbox/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'loupe',
+    'slides.apps.SlidesConfig'
 ]
 
 MIDDLEWARE = [
